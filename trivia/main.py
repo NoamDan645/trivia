659ecca2c9076d903973a966dfb77097b9462cb8
Noam Hadad <noamdan7@gmail.com>
2026-02-15 20:42:21 +0200

    build main and class Quetions

---
+++ trivia/main.py
@@ -0,0 +1,37 @@
+from question import Question
+import pathlib
+import json
+import random
+
+def get_questions_from_file(file_path: str) -> list[Question]:
+    
+    questions_file = pathlib.Path(file_path)
+    
+    assert questions_file.exists(), "The file with questions does not exist."
+    
+    questions_list = json.load(questions_file.open())
+    
+    
+    game_questions = [
+        Question(q["question"], q["answers"], q["correctAnswer"]) 
+        for q in questions_list
+    ]
+    return game_questions
+
+
+if __name__ == "__main__":
+    
+    game_quetions = get_questions_from_file("questions.json")
+    
+    random.shuffle(game_quetions)
+    
+    for q in game_quetions:
+        if q.ask_a_question():
+            print("Correct!")
+        else:
+            print("Wrong!")
+         
+    
+
+
+
---
+++ trivia/question.py
@@ -0,0 +1,40 @@
+class Question:
+    
+    def __init__(self, quetion: str, answers: list[str], correct_index: int):
+        self._my_quetion = quetion
+        self._answers = answers
+        self._correct_index = correct_index
+
+    def print_quetion(self):
+        print(f"Question: {self._my_quetion}")
+
+        for (i,answer) in enumerate(self._answers,1):
+            print(f"{i} - {answer}")
+
+    def ask_a_question(self):
+
+        self.print_quetion()
+
+        answer_index = self._get_answer_from_user()
+
+        return answer_index == self._correct_index
+    
+    def __repr__(self):
+        return f"Question: {self._my_quetion}, Answers: {self._answers}"
+    
+    
+    
+
+    def _get_answer_from_user(self):
+
+        answers_num= len(self._answers)
+
+        while True:
+            try:
+                answer_index = int(input("Enter the number of your answer: "))
+                if 1 <= answer_index <= answers_num:
+                    return answer_index - 1
+                else:
+                    print(f"Please enter a number between 1 and {(answers_num)}.")
+            except ValueError:
+                print("Invalid input. Please enter a number.")
